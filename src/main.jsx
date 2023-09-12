de7145c9bd7142b8eac61725d085558dbedeafca
farhandwi <farhandwi234@gmail.com>
2023-09-12 16:57:56 +0700

    adding redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,51 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import LoginPage from './Pages/login.jsx'
-import RegisterPage from './pages/register.jsx'
-import ErorPage from './pages/404.jsx'
-import ProductsPage from './pages/products'
-import ProfilePage from './pages/profile'
-import DetailProductPage from './pages/detailProduct'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./Pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import ErorPage from "./pages/404.jsx";
+import ProductsPage from "./pages/products";
+import ProfilePage from "./pages/profile";
+import DetailProductPage from "./pages/detailProduct";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <div>Hello World</div>,
-    errorElement: <ErorPage/>
+    errorElement: <ErorPage />,
   },
   {
-    path: '/login',
-    element: <LoginPage/>,
-    errorElement: <ErorPage/>
+    path: "/login",
+    element: <LoginPage />,
+    errorElement: <ErorPage />,
   },
   {
-    path: '/register',
-    element: <RegisterPage/>,
-    errorElement: <ErorPage/>
+    path: "/register",
+    element: <RegisterPage />,
+    errorElement: <ErorPage />,
   },
   {
     path: "/products",
-    element: <ProductsPage/>
+    element: <ProductsPage />,
   },
   {
     path: "/profile",
-    element: <ProfilePage/>
+    element: <ProfilePage />,
   },
   {
     path: "/product/:id",
-    element: <DetailProductPage/>
-  }
-])
+    element: <DetailProductPage />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
